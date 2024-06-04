d6b3697f5366b977137d9129aa60d8bd4106059e
karim <karim.devel@gmail.com>
2024-06-04 17:46:00 -0400

    repositories commit

---
+++ src/main/java/com/bankingsystem/repositories/AccountOperationRepository.java
@@ -0,0 +1,15 @@
+package com.bankingsystem.repositories;
+
+import com.bankingsystem.entities.AccountOperation;
+import org.hibernate.internal.util.StringHelper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AccountOperationRepository extends JpaRepository<AccountOperation,Long> {
+    List<AccountOperation> findByBankAccountId(String accountId);
+    Page<AccountOperation>  findByBankAccountIdOrderByOperationDateDesc(String accountId, Pageable pageable);
+
+}
